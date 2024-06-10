82cf7c93f8fc9779a495ea2017bc01520818761f
LeeNorrell <156039272+LeeNorrell@users.noreply.github.com>
2024-06-10 17:54:15 -0500

    started in the server side changing it over to graphql with apollo server

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,65 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (_, _, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id })
+            }
+
+            throw AuthenticationError;
+        }
+
+    },
+
+    Mutation: {
+        login: async (_, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                return AuthenticationError;
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                return AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addUser: async (_, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (_, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args.bookInfo } },
+                    { new: true }
+                )
+                return updatedUser
+            }
+            throw AuthenticationError
+
+        },
+
+        removeBook: async (_, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user.id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                )
+                return updatedUser
+            }
+            throw AuthenticationError
+        }
+    }
+
+}
